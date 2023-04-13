66247166dfe9fd7b67fafa52a1da09b69b034f78
DavidPalacio99 <dpalacioh@unal.edu.co>
2023-04-13 09:47:16 -0500

    Changing card width style prop

--- src/components/product-list.tsx
+++ src/components/product-list.tsx
@@ -1,8 +1,11 @@
-import { Product } from '@/pages/api/products';
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
-import { Avatar, Card, Row, Col } from 'antd';
-import { useEffect, useState } from 'react';
-
+import { Product } from "@/pages/api/products";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Avatar, Card, Row, Col } from "antd";
+import { useEffect, useState } from "react";
 
 export default function ProductList() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -10,20 +13,20 @@ export default function ProductList() {
     const productsResponse = await fetch("/api/products");
     const formattedProducts = await productsResponse.json();
     setProducts(formattedProducts);
-  }
+  };
   useEffect(() => {
-    init()
-  }, [])
+    init();
+  }, []);
   useEffect(() => {
-    console.log(products)
-  }, [products])
+    console.log(products);
+  }, [products]);
 
   return (
     <Row gutter={[16, 16]}>
       {products.map((product) => (
-        <Col key={product.name} span={6} >
+        <Col key={product.name} span={6}>
           <Card
-            style={{ width: 300 }}
+            style={{ width: 400 }}
             cover={
               <img
                 alt="example"
@@ -37,14 +40,15 @@ export default function ProductList() {
             ]}
           >
             <Card.Meta
-              avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />}
+              avatar={
+                <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+              }
               title={product.name}
               description={product.description}
             />
           </Card>
         </Col>
       ))}
-
     </Row>
-  )
+  );
 }

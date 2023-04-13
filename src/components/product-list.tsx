06607837c607d3bcf82f8146cdf2af054efa85af
Alejandro Chaves <alejandrochvs@alejandrochvs.com>
2023-04-13 10:20:01 -0500

    setup contentful

--- src/components/product-list.tsx
+++ src/components/product-list.tsx
@@ -1,27 +1,25 @@
-import { Product } from '@/pages/api/products';
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
-import { Avatar, Card, Row, Col } from 'antd';
-import { useEffect, useState } from 'react';
+import { Product } from "@/pages/api/products";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Avatar, Card, Row, Col } from "antd";
+import { PropsWithChildren, useEffect } from "react";
 
-
-export default function ProductList() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const init = async () => {
-    const productsResponse = await fetch("/api/products");
-    const formattedProducts = await productsResponse.json();
-    setProducts(formattedProducts);
-  }
+export default function ProductList({
+  products,
+}: PropsWithChildren<{
+  products: Product[];
+}>) {
   useEffect(() => {
-    init()
-  }, [])
-  useEffect(() => {
-    console.log(products)
-  }, [products])
+    console.log(products);
+  }, [products]);
 
   return (
     <Row gutter={[16, 16]}>
       {products.map((product) => (
-        <Col key={product.name} span={6} >
+        <Col key={product.name} span={6}>
           <Card
             style={{ width: 300 }}
             cover={
@@ -37,14 +35,15 @@ export default function ProductList() {
             ]}
           >
             <Card.Meta
-              avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />}
+              avatar={
+                <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+              }
               title={product.name}
               description={product.description}
             />
           </Card>
         </Col>
       ))}
-
     </Row>
-  )
+  );
 }

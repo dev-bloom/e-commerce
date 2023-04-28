2f3ffcb36b3933f1613194887b16d27f6e4120e1
Alejandro Chaves <github@alejandrochaves.dev>
2023-04-28 16:01:30 -0500

    handle 404 product api

--- src/pages/api/product.ts
+++ src/pages/api/product.ts
@@ -2,11 +2,19 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { Product } from "@/types";
 import { getProduct } from "@/utils/api/product.helpers";
 
+interface ErrorResponse {
+  message: string;
+}
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Product>
+  res: NextApiResponse<Product | ErrorResponse>
 ) {
   const product = await getProduct(req.query.slug as string);
+
+  if (!product) {
+    return res.status(404).json({ message: "Product not found" });
+  }
 
   res.status(200).json(product);
 }

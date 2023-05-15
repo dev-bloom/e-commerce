7c8b39e22292df4bec52c67c311d3c8131747a55
Alejandro Chaves <github@alejandrochaves.dev>
2023-05-14 21:11:47 -0500

    fix prodcuts endpoint

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -1,12 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getProducts } from "@/utils/api/product.helpers";
 import { Product } from "@/types";
+import { parseQuery } from "@/utils/helpers";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Product[]>
 ) {
-  const page = req.query.page ? Number(req.query.page) : 1;
+  const page = parseQuery(req.query.page);
   const products = await getProducts({ page });
 
   res.status(200).json(products);

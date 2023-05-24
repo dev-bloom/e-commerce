8649327cc1939e542189fa609dc2d70c87ac7965
David Palacio <118567592+DavidPalacio99@users.noreply.github.com>
2023-05-23 19:34:59 -0500

    Update user reducer
    Co-authored-by: Alejandro Chaves <github@alejandrochaves.dev>

--- src/store/user/user.reducer.ts
+++ src/store/user/user.reducer.ts
@@ -3,14 +3,7 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 import type { User } from "./user.types";
 
 export const setUserReducer = (state: User, action: PayloadAction<User>) => {
-  state.name = action.payload.name;
-  state.email = action.payload.email;
-  state.surname = action.payload.surname;
-  state.address = action.payload.address;
-  state.phone = action.payload.phone;
-  state.city = action.payload.city;
-  state.deparment = action.payload.deparment;
-  state.postalCode = action.payload.postalCode;
+  state = {...state, ...payload}
 };
 
 export const clearUserReducer = (state: User) => {
